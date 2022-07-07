4136d92d63f76931fc920a2782d61829774c291f
danielleelara <dany.sa1@hotmail.com>
2022-07-07 10:00:56 -0300

    feat: criando menu horizontal

--- App.js
+++ App.js
@@ -1,29 +1,12 @@
 import React from 'react';
 import {StatusBar, SafeAreaView} from 'react-native';
-import useProdutores from './src/hooks/useProdutores';
-
-import Home from './src/telas/Produtores/Home';
-import Cesta from './src/telas/Cesta';
-import {NavigationContainer} from '@react-navigation/native';
+import AppRotas from './src/rotas/AppRotas';
 
 export default function App() {
-  const produtores = useProdutores(false);
-
   return (
     <SafeAreaView style={{flex: 1}}>
-      <NavigationContainer>
-        <StatusBar />
-        <Home melhoresProdutores={true} />
-        {/* {produtores.length > 0 && (
-        <Cesta
-          produtor={{
-            nome: produtores[0].nome,
-            imagem: produtores[0].imagem,
-          }}
-          {...produtores[0].cestas[0]}
-        />
-      )} */}
-      </NavigationContainer>
+      <StatusBar />
+      <AppRotas />
     </SafeAreaView>
   );
 }
---
+++ src/rotas/AppRotas.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import MelhoresProdutores from '../telas/Produtores/MelhoresProdutores';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import ProdutorRotas from './ProdutorRotas';
+
+const Tab = createBottomTabNavigator();
+
+export default function AppRotas() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={ProdutorRotas} />
+        <Tab.Screen name="Melhores Produtores" component={MelhoresProdutores} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
